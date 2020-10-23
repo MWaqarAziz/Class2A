9f2a5b69312487034bfa04c744ac5c153e4ae2d4
m.waqaraziz@yahoo.com <m.waqaraziz@yahoo.com>
2020-10-24 01:50:22 +0500

    One Day

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
        
         <h1>Tommorrow,We will be starting working on React.I hope God would help to sort out my problems</h1>
-        
+        <h1>One day,I will prove myself.</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
